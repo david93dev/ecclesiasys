7813105a18d3556731b262068fa2722d74cf2dd2
João Felipe <125924177+joaofelipe05@users.noreply.github.com>
2026-04-10 22:34:32 -0300

    Adicionando start para deploy no Render

--- backend/src/server.js
+++ backend/src/server.js
@@ -32,8 +32,8 @@ app.get("/", (req, res)=> {
     res.send("Projeto rodando!")
 })
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`)
+  console.log(`Servidor rodando na porta ${PORT}`)
 })
